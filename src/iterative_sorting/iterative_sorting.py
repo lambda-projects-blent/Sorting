69f0c3e65dcc5aa904b2f79ed1057ee96d617d6b
Brandon <Blent1050@yahoo.com>
2019-05-03 11:06:00 -0400

    completed recursive sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,36 +1,43 @@
-# TO-DO: complete the helper function below to merge 2 sorted arrays
-def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = []
-    indexA = 0
-    indexB = 0
-    for i in range(elements):
-        if indexA >= len(arrA):
-            merged_arr.extend(arrB[indexB:])
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
+    for i in range(0, len(arr) - 1):
+        cur_index = i
+        smallest_index = cur_index
+
+        for j in range(cur_index, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
+
+    return arr
+
+
+# TO-DO:  implement the Bubble Sort function below
+
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        j = 0
+        swap_occurred = False
+
+        while j < len(arr) - 1:
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swap_occurred = True
+            j += 1
+
+        if swap_occurred is False:
             break
-        elif indexB >= len(arrB):
-            merged_arr.extend(arrA[indexA:])
-            break
-        elif arrA[indexA] < arrB[indexB]:
-            merged_arr.append(arrA[indexA])
-            indexA += 1
-        else:
-            merged_arr.append(arrB[indexB])
-            indexB += 1
 
-    return merged_arr
+    return arr
 
 
-def merge_sort(arr):
-    # TO-DO
-    if len(arr) > 1:
-        left = merge_sort(arr[0: int(len(arr) / 2)])
-        right = merge_sort(arr[int(len(arr) / 2):])
+# STRETCH: implement the Count Sort function below
+def count_sort(arr, maximum=-1):
 
-        arr = merge(left, right)
     return arr
 
 
 arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
 
-print(merge_sort(arr1))
+
+print(bubble_sort(arr1))
